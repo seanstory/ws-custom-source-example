8e965d0cadcccf38a3886fd015d1ab0ca5bc0da3
Sean Story <sean.j.story@gmail.com>
2020-04-09 15:44:25 -0500

    Mocked the server

--- ws-filesystem-core/src/test/java/com/sstory/source/filesystem/run/SyncTest.groovy
+++ ws-filesystem-core/src/test/java/com/sstory/source/filesystem/run/SyncTest.groovy
@@ -4,19 +4,38 @@ import com.sstory.workplace.search.sdk.run.Sync
 import org.junit.Rule
 import org.junit.contrib.java.lang.system.ExpectedSystemExit
 import org.junit.contrib.java.lang.system.internal.CheckExitCalled
-import spock.lang.Ignore
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.model.Header
+import org.mockserver.model.HttpRequest
+import org.mockserver.model.HttpResponse
+import spock.lang.Shared
 import spock.lang.Specification
 
-//@Ignore //TODO - mock out network/client stuff so that this can be run
 class SyncTest extends Specification {
 
     @Rule
-    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none()
+
+    @Shared
+    private ClientAndServer mockServer;
+
+    def setupSpec() {
+        mockServer = ClientAndServer.startClientAndServer(3002)
+    }
+
+    def cleanupSpec() {
+        mockServer.stop()
+    }
 
     def "test the sync"(){
         setup:
         def yamlPath = "src/test/resources/test.yml"
+        def contentSourceKey = "5e8f5266f74c321dae6e5548"
         def sourceName = "helloworld"
+
+        def req = HttpRequest.request().withMethod("POST").withPath("/api/ws/v1/sources/${contentSourceKey}/documents/bulk_create.json")
+        def res = HttpResponse.response().withStatusCode(200).withBody('{"very":"nice"}').withHeaders(new Header("Content-Type", "application/json; charset=utf-8"))
+        mockServer.when(req).respond(res)
 
         when:
         exit.expectSystemExit()
---
+++ ws-filesystem-core/src/test/resources/logback.groovy
@@ -0,0 +1,14 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import static ch.qos.logback.classic.Level.*
+
+def PATTERN = "%d %highlight(%-5level) [%thread] %logger{36} - %msg%n"
+
+appender("console", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) { pattern = PATTERN }
+}
+
+logger("com.sstory", DEBUG)
+logger("com.sstory.workplace.search.client", TRACE)
+logger("org.mockserver", WARN)
+root(INFO, ["console"])
